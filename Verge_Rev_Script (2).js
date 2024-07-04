fdd7a6313e0ce1fe5f72ae06ae4d21dc86e94835
xlh8394 <94887643+xlh8394@users.noreply.github.com>
2024-07-04 12:11:57 +0800

    Update Verge_Rev_Script (2).js

--- Verge_Rev_Script (2).js
+++ Verge_Rev_Script (2).js
@@ -317,7 +317,7 @@ function main(config) {
       "type": "url-test",
       "tolerance": 0,
       "include-all": true,
-      "filter": "(?i)🇨🇳|🇹🇼|台湾|(\b(TW|Tai|Taiwan)\b)",
+      "filter": "(?i)🇨🇳|🇹🇼|台湾|Taipei|(\b(TW|Tai|Taiwan)\b)",
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/China.png"
     }
   ];
